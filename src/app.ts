fac13525dd76f3226a9e4266df531c3baf2a1504
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2019-07-23 23:14:26 +0200

    Install socket.io and implement a basic method

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,21 @@ import { routes, server } from './constants';
 import { conversationController } from './conversation';
 import { userController } from './users';
 import httpErrors from 'http-errors-express';
+import * as socketIO from 'socket.io';
 const app = express();
+const io = socketIO(8080);
 
 app.use(bodyParser.json());
 
 app.post(routes.addUser, userController.addUser);
 
 app.post(routes.addMessage, conversationController.addMessage);
+
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    io.emit('user disconect');
+  });
+});
 
 app.use(httpErrors());
 
