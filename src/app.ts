d86e412d608641a2a1023763a0dfa1bc58a6f9e8
AntonioContrerasRomán <antoniocontrerasroman@gmail.com>
2019-08-05 23:04:49 +0200

    Add in app userIdExist

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,10 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import httpErrors from 'http-errors-express';
 import * as socketIO from 'socket.io';
-import { socketEvents, routes, server } from './constants';
-import { conversationController } from './conversation';
-import { userController } from './users';
 import { Socket } from 'socket.io';
+import { routes, server, socketEvents } from './constants';
+import { conversationController } from './conversation';
+import { service, userController } from './users';
 const app = express();
 const io = socketIO(8080);
 
@@ -17,7 +17,9 @@ app.post(routes.addMessage, conversationController.addMessage);
 
 io.use((socket, next) => {
   const { userId } = socket.handshake.query;
-
+  if (service.userIdExist(userId)) {
+    return next();
+  }
 });
 
 io.on(socketEvents.connection, (socket: Socket) => {
